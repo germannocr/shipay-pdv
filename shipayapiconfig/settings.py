0b7f79861c0209a7d81579f61414e0c9eefcab7a
Germanno Correia <germanno.correia@simbioseventures.com>
2020-08-25 15:39:58 -0300

    Add app name to settings

--- shipayapiconfig/settings.py
+++ shipayapiconfig/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shipayapi',
     'rest_framework',
     'django.contrib.sites',
+    'shipayapi'
 ]
 
 MIDDLEWARE = [
